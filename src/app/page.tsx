924707e786f9334b4371ae39e937a2f0fa4828bf
Shakti Priya <priyashakti68@gmail.com>
2025-05-24 20:33:11 +0530

    add initial configuration files

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+"use client"
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold text-center mt-52">Hello world;</h1>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+     hey navvy 
+    </div>
+  );
+}
+
+export default Navbar;
